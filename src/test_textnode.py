b40f6289fd034f518f72529aa0a4a77e284d9b76
lazynex <lazynex@gmail.com>
2025-05-11 22:35:27 +0300

    Added text_node_to_html_node and split_nodes_delimiter functions with respective unit tests.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +23,42 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is not a text node", TextType.CODE)
         node2 = TextNode("This is a text node", TextType.CODE)
         self.assertNotEqual(node, node2)
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_split_nopdes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        print(new_nodes)
+        test_nodes = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, test_nodes)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -22,4 +23,41 @@ class TextNode():
         return False
 
     def __repr__(self):
-        return f'TextNode({self.text}, {self.text_type.value}, {self.url})'
+        return f'TextNode("{self.text}", {self.text_type.value}, "{self.url}")'
+
+    
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode('b', text_node.text)
+        case TextType.ITALIC:
+            return LeafNode('i', text_node.text)
+        case TextType.CODE:
+            return LeafNode('code', text_node.text)
+        case TextType.LINK:
+            return LeafNode('a', text_node.text, {'href': text_node.url})
+        case TextType.IMAGE:
+            return LeafNode('img', '', props={'src': text_node.url, 'alt': text_node.text})
+    raise Exception('Unknown TextType value')
+    
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    nodes = []
+    for n in old_nodes:
+        if n.text_type is not TextType.TEXT:
+            nodes.append(old_nodes)
+            continue
+
+        blocks = n.text.split(delimiter)
+        
+        if len(blocks) != 3:
+            raise Exception(f'Invalid Markdown: no closing "{delimiter}"')
+        
+        nodes = [
+            TextNode(blocks[0], TextType.TEXT), 
+            TextNode(blocks[1], text_type), 
+            TextNode(blocks[2], TextType.TEXT), 
+        ]
+    return nodes
